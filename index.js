c7aea5dfee599aae0fa7d5d633dc85d48024e538
Kudoozz <dineshkumar73827@gmail.com>
2024-12-08 14:36:22 +0530

    url changed

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ connectToDatabase()
 
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin:"https://ems-frontend-virid.vercel.app",
+    credentials:true
+}))
 app.use(express.json())
 app.use(express.static('public/uploads'))
 
